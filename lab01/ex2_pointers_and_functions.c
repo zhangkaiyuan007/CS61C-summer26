407a461ceefe7369a0348f4fbb269430a157ca77
Adelina Chau <73216866+adelly13@users.noreply.github.com>
2026-06-25 00:00:23 -0700

    added lab01 starter code (#1)

---
+++ lab01/ex2_pointers_and_functions.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void add_one(int input) {
+  input += 1;
+}
+
+// TODO: create a pointer to input
+void add_one_pointer(______ input) {
+  // TODO: add one to the integer that input points to
+  ________ += 1;
+}
+
+// TODO: create a pointer to a pointer to input
+void add_one_double_ptr(______ input) {
+  // TODO: add one to the integer that input doubly points to
+  ________ += 1;
+}
+
+int main() {
+  // Assign x (an integer) to 5
+  int x = 5;
+
+  // Call add_one on x
+  add_one(x);
+
+  // This line should print 5
+  // Why doesn't this work?
+  printf("add_one: %d\n", x);
+
+  // Let's try using add_one_pointer
+
+  // TODO: use add_one_pointer to increment x
+  // Hint: compare the type of x with the type of the argument
+  //       for add_one_pointer
+  add_one_pointer(______);
+
+  // This line should print 6
+  printf("add_one_pointer: %d\n", x);
+  
+  // TODO: Let's save the pointer to x in y
+  ______ y = ______;
+
+  // TODO: use add_one_double_ptr to increment x again, using the pointer we just made
+  add_one_double_ptr(______);
+
+  // This line should print 7
+  printf("add_one_double_ptr: %d\n", x);
+
+  return 0;
+}
