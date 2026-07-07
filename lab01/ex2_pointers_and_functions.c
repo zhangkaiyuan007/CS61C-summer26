c540919d3dd194c319beadb834495de72d06365b
kyzhang <robert_kaiyuan@outlook.com>
2026-07-08 00:12:47 +0800

    lab01

--- lab01/ex2_pointers_and_functions.c
+++ lab01/ex2_pointers_and_functions.c
@@ -5,15 +5,15 @@ void add_one(int input) {
 }
 
 // TODO: create a pointer to input
-void add_one_pointer(______ input) {
+void add_one_pointer(int* input) {
   // TODO: add one to the integer that input points to
-  ________ += 1;
+  *input += 1;
 }
 
 // TODO: create a pointer to a pointer to input
-void add_one_double_ptr(______ input) {
+void add_one_double_ptr(int** input) {
   // TODO: add one to the integer that input doubly points to
-  ________ += 1;
+  **input += 1;
 }
 
 int main() {
@@ -32,16 +32,16 @@ int main() {
   // TODO: use add_one_pointer to increment x
   // Hint: compare the type of x with the type of the argument
   //       for add_one_pointer
-  add_one_pointer(______);
+  add_one_pointer(&x);
 
   // This line should print 6
   printf("add_one_pointer: %d\n", x);
   
   // TODO: Let's save the pointer to x in y
-  ______ y = ______;
+  int* y = &x;
 
   // TODO: use add_one_double_ptr to increment x again, using the pointer we just made
-  add_one_double_ptr(______);
+  add_one_double_ptr(&y);
 
   // This line should print 7
   printf("add_one_double_ptr: %d\n", x);
